5dc0a7103c68da639a86bcb26e2ba411bf2924e1
xiaoniu1234 <2039210460@qq.com>
2022-06-17 21:54:21 +0800

    TestUpload

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 840,
+      height: 500,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        enableRemoteModule: true
+      }  
+    })
+    const remote = require('@electron/remote/main')
+    remote.initialize()
+    remote.enable(win.webContents)
+    win.setIcon("./src/appicon_512_transparent.png")
+    win.title = "Anime Game UA Patch Tool"
+    win.resizable = false
+    win.loadFile('./src/index.html')
+}
+
+app.whenReady().then(() => {
+    console.log("running")
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const { dialog } = require('@electron/remote');
+const fs = require('fs')
+
+var file_path
+
+function select_ua_file(){
+    file_path = dialog.showOpenDialogSync({
+        title:'Select UserAssembly.dll',
+        filters:[{
+        name: 'UserAssembly.dll',
+        extensions: ['dll']
+        }]
+    });
+    if(file_path){
+        document.getElementById("select_path_view").innerHTML = "Path: " + file_path[0];
+    }
+}
+
+function cs_dialog(){
+    dialog.showMessageBox({
+        type: "info",
+        title: "Confidentiality statement",
+        message: "just don't casually send this software to people you don't trust.",
+        buttons: ["OK"]
+    })
+}
+
+function is_dialog(){
+    dialog.showMessageBox({
+        type: "info",
+        title: "Instructions",
+        message: "This program is the best instruction.",
+        buttons: ["OK"]
+    })
+}
+
+function test_(){
+    const data = fs.readFileSync(file_path[0])
+    const data_buff = Buffer.from(data)
+    data_buff.fill(Buffer.from("90","hex"), start=0x018C0410, end=0x018C0411)
+    data_buff.fill(Buffer.from("90","hex"), start=0x018C0411, end=0x018C0412)
+    data_buff.fill(Buffer.from("B0","hex"), start=0x029A3C60, end=0x029A3C61)
+    data_buff.fill(Buffer.from("01","hex"), start=0x029A3C61, end=0x029A3C62)
+    data_buff.fill(Buffer.from("C3","hex"), start=0x029A3C62, end=0x029A3C63)
+    fs.writeFileSync(file_path[0], data_buff)
+    dialog.showMessageBox({
+        type: "info",
+        title: "666",
+        message: "尊贵的SCP10388，您的UA文件我已经帮你Patch了，Only for you.",
+        buttons: ["OK"]
+    })
+}
